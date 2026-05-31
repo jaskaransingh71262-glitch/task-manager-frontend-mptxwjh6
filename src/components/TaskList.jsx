1d0ab903f6171409bd558fe816867d1c7a83e34c
jaskaransingh71262-glitch <jaskaransingh71262@gmail.com>
2026-05-31 21:05:21 +0530

    Add src/components/TaskList.jsx

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const TaskList = ({ tasks }) => {
+  return (
+    <ul className="list-group">
+      {tasks.map((task) => (
+        <li key={task.id} className="list-group-item">
+          <h5>{task.title}</h5>
+          <p>{task.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default TaskList;
